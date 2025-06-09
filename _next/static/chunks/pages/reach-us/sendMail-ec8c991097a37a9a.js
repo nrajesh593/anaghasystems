9972ab54b7655ee6abe762eb00e10cf4a2f9b6c4
rajesh <rajesh@petacrate.com>
2025-06-09 06:40:04 +0530

    initial commit

---
+++ _next/static/chunks/pages/reach-us/sendMail-ec8c991097a37a9a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4974],{4188:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/reach-us/sendMail",function(){return e(4241)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,6593,8792],()=>n(4188)),_N_E=_.O()}]);
